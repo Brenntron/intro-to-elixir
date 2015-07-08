86a2b9bb3324f2fa5be5d0e47033684c3b704a7e
Brennan Willingham <brennanw22@gmail.com>
2015-07-08 01:07:21 -0500

    Finished GenEvent section. Added managers and turned the server state to a map. This will make improving the registry easier in the future.

--- test/intro_to_elixir/registry_test.exs
+++ test/intro_to_elixir/registry_test.exs
@@ -1,8 +1,20 @@
 defmodule IntroToElixir.RegistryTest do
   use ExUnit.Case, async: true
 
+  defmodule Forwarder do
+    use GenEvent
+
+    def handle_event(event, parent) do
+      send parent, event
+      {:ok, parent}
+    end
+  end
+
   setup do
-    {:ok, registry} = IntroToElixir.Registry.start_link
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = IntroToElixir.Registry.start_link(manager)
+
+    GenEvent.add_mon_handler(manager, Forwarder, self())
     {:ok, registry: registry}
   end
 
@@ -21,5 +33,14 @@ defmodule IntroToElixir.RegistryTest do
     {:ok, bucket} = IntroToElixir.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
     assert IntroToElixir.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "sends events on create and crash", %{registry: registry} do
+    IntroToElixir.Registry.create(registry, "shopping")
+    {:ok, bucket} = IntroToElixir.Registry.lookup(registry, "shopping")
+    assert_receive {:create, "shopping", ^bucket}
+
+    Agent.stop(bucket)
+    assert_receive {:exit, "shopping", ^bucket}
   end
 end
